9b43e6c01fc0893e96d34d8a2b71a1347af4e138
Sunil Kumar Rai <srai@hawk.iit.edu>
2018-02-05 23:55:56 -0600

    Initial commit

---
+++ src/knn/KNN_Distance.java
@@ -0,0 +1,27 @@
+package knn;
+
+/*
+ * This class is for calculating  Euclidean and Manhattan distance. 
+ *   */
+
+public class KNN_Distance {
+	
+	public double getEuclideanDistance( double[] features1,  double[] features2) {
+        double sum = 0;
+        for (int i = 0; i < features1.length; i++)
+        {  //System.out.println(features1[i]+" "+features2[i]);
+        	//applied Euclidean distance formula
+            sum += Math.pow(features1[i] - features2[i], 2);
+        }return Math.sqrt(sum);
+    }
+	
+	public double getManhattanDistance(final double[] features1, final double[] features2) {
+        double sum = 0;
+        for (int i = 0; i < features1.length; i++)
+        	//Applied Manhattan distance formula
+            sum += Math.abs(features1[i] - features2[i]);
+        return sum;
+    }
+
+
+}
